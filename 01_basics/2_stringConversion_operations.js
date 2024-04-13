52cdde52f49f999d43c7b85552476740da19ed29
Sameer Dixit <dixitsameer107@gmail.com>
2024-04-13 19:08:23 +0000

    String conversion confusions and solutions with the code

---
+++ 01_basics/2_stringConversion_operations.js
@@ -0,0 +1,9 @@
+// console.log(1+2);
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+// console.log(+true);    gives 1
+// console.log(true+);       error or not allowed
+// console.log(+"");   gives 0
+
