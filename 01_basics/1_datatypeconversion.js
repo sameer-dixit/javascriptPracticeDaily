467a5ed7012d94cd8784efbdc6ec369527d5bc24
Sameer Dixit <dixitsameer107@gmail.com>
2024-04-13 11:13:43 +0000

    learnt about datatypeconversion Basics

---
+++ 01_basics/1_datatypeconversion.js
@@ -0,0 +1,46 @@
+// let score="33abc";
+// let score=null;
+
+// let score=undefined;
+
+// let score=true;
+// let score="sameer";
+
+// console.log(typeof(score));
+// score=Number(score);
+// console.log(typeof(score));
+// console.log(score);
+
+
+// "33"=>33
+// "33abc"=>NAN
+// null =>0
+// undefined=>NAN
+// true=> 1
+// "sameer"=>NAN
+
+
+
+
+// number to boolean 
+
+// let islogIn="sameer";
+// let booleanislogIn=Boolean(islogIn);
+// console.log(booleanislogIn);
+
+
+// 1=>true
+// ""=>false
+// "sameer"=> true
+
+// number to string
+let num=1;
+let strnum=String(num);
+console.log(strnum);
+console.log(typeof strnum);
+
+
+// 1=>1 and type of string 
+// 1ab=> error 
+// true=> string
+// tru1=>error not defined 
