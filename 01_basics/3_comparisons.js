7902d3ae33c4c65b6904b2fac13d7654db61f289
Sameer Dixit <dixitsameer107@gmail.com>
2024-04-13 19:29:01 +0000

    Strict check and comoarision of null and undefined

---
+++ 01_basics/3_comparisons.js
@@ -0,0 +1,10 @@
+// console.log("2">0); gives true automatically converts the string to number
+// console.log(null==0); gives false
+// console.log(null>0); gives false because 0!>0
+// console.log(null>=0);  gives true because comparision acts differently and converts 
+// the null to a number InputEvent.apply. 0 and then compares it 
+
+
+// strict check 
+// console.log("2"===2);  gives false checks the equality without converting and compares not only the value 
+// but also the DataType
