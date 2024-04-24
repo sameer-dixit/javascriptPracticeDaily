9e8ec79f79e44e2ba032f9db5125da9a8578ce77
Sameer Dixit <dixitsameer107@gmail.com>
2024-04-24 18:26:25 +0000

    Date time and time stamps in detail how date object works

---
+++ 01_basics/7_dateandTimedepth.js
@@ -0,0 +1,24 @@
+let dt=new Date();
+// console.log(dt);
+// console.log(dt.toString());
+// console.log(dt.toDateString());
+// console.log(dt.toTimeString());
+// console.log(dt.toISOString());
+// console.log(dt.toLocaleString());
+
+// let mydate=new Date(2023,0,23);
+// let mydate=new Date("2023-01-23");
+
+// console.log(mydate.toLocaleString());
+
+
+
+
+// Time stamps
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+
+
+// 
+
+
