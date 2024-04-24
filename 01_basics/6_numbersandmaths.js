9c4319a084c81152872c03d10155a71c85972152
Sameer Dixit <dixitsameer107@gmail.com>
2024-04-24 17:48:29 +0000

    numbers and math functionalities

---
+++ 01_basics/6_numbersandmaths.js
@@ -0,0 +1,21 @@
+// const num=600;
+
+
+// const numb=new Number(400000);
+// console.log(num);
+// console.log(numb);
+// console.log(numb.toFixed(1));
+// // converts the number seperated with us standrads counting
+// console.log(numb.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++
+console.log(Math);
+// minus value will become positive
+console.log(Math.abs(-4));
+console.log(Math.random());
+// to display the random number around min and max
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
